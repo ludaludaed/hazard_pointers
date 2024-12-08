bb008a06a4cb247cc12dd3a75adf90e87230563b
ludaludaed <tagirov_2003@bk.ru>
2024-12-08 04:06:45 +0300

    Update

--- test/main.cpp
+++ test/main.cpp
@@ -16,86 +16,9 @@
 #include <ostream>
 #include <stdexcept>
 #include <string>
-#include <string_view>
 #include <thread>
-#include <utility>
 #include <vector>
 
-
-struct A : lu::forward_list_base_hook<> {
-    int a;
-};
-
-void slist_test() {
-    auto comp = [](const A &left, const A &right) {
-        return left.a < right.a;
-    };
-
-    std::vector<A>
-            vec(20);
-    for (int i = 0; i < vec.size(); ++i) {
-        vec[i].a = rand() % 100;
-    }
-
-    std::reverse(vec.begin(), vec.end());
-
-    lu::forward_list<A> list_a;
-    lu::forward_list<A> list_b;
-
-    for (int i = 0; i < 10; ++i) {
-        list_a.push_front(vec[i]);
-    }
-
-    for (int i = 10; i < 20; ++i) {
-        list_b.push_front(vec[i]);
-    }
-
-    for (auto it = list_a.begin(); it != list_a.end(); ++it) {
-        std::cout << it->a << " ";
-    }
-
-    std::cout << std::endl;
-
-    for (auto it = list_b.begin(); it != list_b.end(); ++it) {
-        std::cout << it->a << " ";
-    }
-
-    std::cout << std::endl;
-
-    list_a.sort(comp);
-    list_b.sort(comp);
-
-    std::swap(list_a, list_b);
-
-    for (auto it = list_a.begin(); it != list_a.end(); ++it) {
-        std::cout << it->a << " ";
-    }
-
-    std::cout << std::endl;
-
-    for (auto it = list_b.begin(); it != list_b.end(); ++it) {
-        std::cout << it->a << " ";
-    }
-
-    std::cout << std::endl;
-
-    list_a.merge(list_b, comp);
-
-    for (auto it = list_a.begin(); it != list_a.end(); ++it) {
-        std::cout << it->a << " ";
-    }
-
-    std::cout << std::endl;
-
-    for (auto it = list_b.begin(); it != list_b.end(); ++it) {
-        std::cout << it->a << " ";
-    }
-
-    std::cout << std::endl;
-
-    std::cout << list_a.empty() << " " << list_b.empty() << std::endl;
-    std::cout << list_a.size() << " " << list_b.size() << std::endl;
-}
 
 template<typename TContainer>
 void stressTest(int actions, int threads) {
@@ -113,30 +36,22 @@ void stressTest(int actions, int threads) {
                     generated[i].push_back(a);
                 } else {
                     auto a = container.pop();
-                    if (a) {
-                        extracted[i].push_back(*a);
-                    }
+                    if (a) { extracted[i].push_back(*a); }
                 }
             }
         });
     }
 
-    for (auto &thread: workers) {
-        thread.join();
-    }
+    for (auto &thread: workers) { thread.join(); }
 
     std::vector<int> all_generated;
     std::vector<int> all_extracted;
     for (int i = 0; i < generated.size(); i++) {
-        for (int j = 0; j < generated[i].size(); j++) {
-            all_generated.push_back(generated[i][j]);
-        }
+        for (int j = 0; j < generated[i].size(); j++) { all_generated.push_back(generated[i][j]); }
     }
 
     for (int i = 0; i < extracted.size(); i++) {
-        for (int j = 0; j < extracted[i].size(); j++) {
-            all_extracted.push_back(extracted[i][j]);
-        }
+        for (int j = 0; j < extracted[i].size(); j++) { all_extracted.push_back(extracted[i][j]); }
     }
 
     while (true) {
@@ -153,7 +68,8 @@ void stressTest(int actions, int threads) {
     std::sort(all_extracted.begin(), all_extracted.end());
     for (int i = 0; i < all_extracted.size(); i++) {
         if (all_generated[i] != all_extracted[i]) {
-            throw std::runtime_error("the values must be equal: " + std::to_string(all_generated[i]) + ", " + std::to_string(all_extracted[i]));
+            throw std::runtime_error("the values must be equal: " + std::to_string(all_generated[i]) + ", "
+                                     + std::to_string(all_extracted[i]));
         }
     }
 }
@@ -161,9 +77,7 @@ void stressTest(int actions, int threads) {
 template<class Func>
 void abstractStressTest(Func &&func) {
     std::size_t num_of_threads = std::thread::hardware_concurrency();
-    for (int i = 1; i <= num_of_threads; i++) {
-        std::cout << "\t" << i;
-    }
+    for (int i = 1; i <= num_of_threads; i++) { std::cout << "\t" << i; }
     // num_of_threads = 1;
     std::cout << std::endl;
     for (int i = 500000; i <= 6000000; i += 500000) {
@@ -178,13 +92,15 @@ void abstractStressTest(Func &&func) {
         lu::detach_thread();
     }
     if (lu::get_default_domain().num_of_reclaimed() != lu::get_default_domain().num_of_retired()) {
-        throw std::runtime_error("the number of reclaimed and retired must be equal: " + std::to_string(lu::get_default_domain().num_of_reclaimed()) + ", " + std::to_string(lu::get_default_domain().num_of_retired()));
+        throw std::runtime_error("the number of reclaimed and retired must be equal: "
+                                 + std::to_string(lu::get_default_domain().num_of_reclaimed()) + ", "
+                                 + std::to_string(lu::get_default_domain().num_of_retired()));
     }
 }
 
 int main() {
     for (int i = 0; i < 1000; ++i) {
         std::cout << "iteration: #" << i << std::endl;
-        abstractStressTest(stressTest<lu::hp::MSQueue<int, lu::EmptyBackOff>>);
+        abstractStressTest(stressTest<lu::hp::TreiberStack<int, lu::EmptyBackOff>>);
     }
 }
