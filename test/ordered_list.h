3cffa789625883f994e3d01db3b7c167b96c0884
ludaludaed <tagirov_2003@bk.ru>
2024-12-10 20:31:30 +0300

    Update

--- test/ordered_list.h
+++ test/ordered_list.h
@@ -2,6 +2,7 @@
 #define __ORDERED_LIST_H__
 
 #include <back_off.h>
+#include <cstddef>
 #include <hazard_pointer.h>
 #include <marked_ptr.h>
 
@@ -9,6 +10,18 @@
 
 
 namespace lu {
+    template<class ValueType>
+    struct OrderedListNode : public lu::hazard_pointer_obj_base<OrderedListNode<ValueType>> {
+    public:
+        template <class... Args>
+        explicit OrderedListNode(Args&&... args)
+            : value(std::forward<Args>(args)...) {}
+
+    public:
+        ValueType value;
+        std::atomic<lu::marked_ptr<OrderedListNode>> next{};
+    };
+
     template<class ValueType, class KeyCompare, class KeySelect, class BackOff>
     class OrderedList : private lu::detail::EmptyBaseHolder<KeyCompare>,
                         private lu::detail::EmptyBaseHolder<KeySelect> {
@@ -16,47 +29,44 @@ namespace lu {
         using KeyCompareHolder = lu::detail::EmptyBaseHolder<KeyCompare>;
         using KeySelectHolder = lu::detail::EmptyBaseHolder<KeySelect>;
 
-        struct Node : public lu::hazard_pointer_obj_base<Node> {
-        public:
-            template<class... Args>
-            explicit Node(Args &&...args)
-                : value(std::forward<Args>(args)...) {}
+        using node_type = OrderedListNode<ValueType>;
 
-        public:
-            ValueType value;
-            std::atomic<lu::marked_ptr<Node>> next{};
-        };
-
-        using node_pointer = Node *;
-        using node_marked_pointer = lu::marked_ptr<Node>;
+        using node_ptr = node_type *;
+        using node_marked_ptr = lu::marked_ptr<node_type>;
 
         struct position {
-            node_pointer cur;
-            node_pointer next;
-            std::atomic<node_marked_pointer> *prev_pointer;
+            node_ptr cur;
+            node_ptr next;
+            std::atomic<node_marked_ptr> *prev_pointer;
 
             lu::hazard_pointer cur_guard{lu::make_hazard_pointer()};
             lu::hazard_pointer next_guard{lu::make_hazard_pointer()};
             lu::hazard_pointer prev_guard{lu::make_hazard_pointer()};
         };
 
+        template<class Types>
         class Iterator {
         friend class OrderedList;
 
         public:
+            using value_type = typename Types::value_type;
+            using difference_type = typename Types::difference_type;
+            using pointer = typename Types::pointer;
+            using reference = typename Types::reference;
             using iterator_category = std::forward_iterator_tag;
-            using value_type = ValueType;
-            using difference_type = std::ptrdiff_t;
-            using pointer = const ValueType*;
-            using reference = const ValueType&;
+
+            using node_ptr = typename Types::node_pointer;
+            using list_ptr = typename Types::list_ptr;
 
         private:
-            Iterator(lu::hazard_pointer guard, node_pointer current, OrderedList* list) noexcept
+            Iterator(lu::hazard_pointer guard, node_ptr current, list_ptr list) noexcept
                 : guard_(std::move(guard))
                 , current_(current)
                 , list_(list) {}
 
         public:
+            Iterator() = default;
+
             Iterator(const Iterator& other) noexcept
                 : guard_(lu::make_hazard_pointer())
                 , current_(other.current_)
@@ -112,7 +122,7 @@ namespace lu {
         private:
             void increment() noexcept {
                 auto next_guard = lu::make_hazard_pointer();
-                auto next = next_guard.protect(current_->next, [](node_marked_pointer ptr) { return ptr.get(); });
+                auto next = next_guard.protect(current_->next, [](node_marked_ptr ptr) { return ptr.get(); });
                 if (next.get_bit()) {
                     position new_pos;
                     list_->find(current_->value, new_pos);
@@ -126,8 +136,8 @@ namespace lu {
 
         private:
             lu::hazard_pointer guard_{};
-            node_pointer current_{};
-            OrderedList* list_{};
+            node_ptr current_{};
+            list_ptr list_{};
         };
 
     public:
@@ -144,10 +154,10 @@ namespace lu {
 
     private:
         static bool unlink(position &pos) {
-            node_marked_pointer next(pos.next);
-            if (pos.cur->next.compare_exchange_weak(next, node_marked_pointer(next.get(), 1))) {
-                node_marked_pointer cur(pos.cur);
-                if (pos.prev_pointer->compare_exchange_weak(cur, node_marked_pointer(pos.next))) {
+            node_marked_ptr next(pos.next);
+            if (pos.cur->next.compare_exchange_weak(next, node_marked_ptr(next.get(), 1))) {
+                node_marked_ptr cur(pos.cur);
+                if (pos.prev_pointer->compare_exchange_weak(cur, node_marked_ptr(pos.next))) {
                     cur->retire();
                 }
                 return true;
@@ -155,10 +165,10 @@ namespace lu {
             return false;
         }
 
-        static bool link(position &pos, node_pointer new_node) {
-            node_marked_pointer cur(pos.cur);
+        static bool link(position &pos, node_ptr new_node) {
+            node_marked_ptr cur(pos.cur);
             new_node->next.store(cur);
-            if (pos.prev_pointer->compare_exchange_weak(cur, node_marked_pointer(new_node))) {
+            if (pos.prev_pointer->compare_exchange_weak(cur, node_marked_ptr(new_node))) {
                 return true;
             } else {
                 new_node->next.store({});
@@ -167,17 +177,17 @@ namespace lu {
         }
 
         template<class Compare>
-        static bool find(std::atomic<node_marked_pointer> *head, const value_type &value, position &pos,
+        static bool find(std::atomic<node_marked_ptr> *head, const value_type &value, position &pos,
                          Compare &&comp) {
-            std::atomic<node_marked_pointer> *prev_pointer;
-            node_marked_pointer cur{};
+            std::atomic<node_marked_ptr> *prev_pointer;
+            node_marked_ptr cur{};
 
             BackOff back_off;
 
         try_again:
             prev_pointer = head;
 
-            cur = pos.cur_guard.protect(*head, [](node_marked_pointer ptr) { return ptr.get(); });
+            cur = pos.cur_guard.protect(*head, [](node_marked_ptr ptr) { return ptr.get(); });
 
             while (true) {
                 if (!cur) {
@@ -187,8 +197,8 @@ namespace lu {
                     return false;
                 }
 
-                node_marked_pointer next
-                        = pos.next_guard.protect(cur->next, [](node_marked_pointer ptr) { return ptr.get(); });
+                node_marked_ptr next
+                        = pos.next_guard.protect(cur->next, [](node_marked_ptr ptr) { return ptr.get(); });
 
                 if (prev_pointer->load().all() != cur.get()) {
                     back_off();
@@ -196,8 +206,8 @@ namespace lu {
                 }
 
                 if (next.get_bit()) {
-                    node_marked_pointer not_marked_cur(cur.get(), 0);
-                    if (prev_pointer->compare_exchange_weak(not_marked_cur, node_marked_pointer(next.get(), 0))) {
+                    node_marked_ptr not_marked_cur(cur.get(), 0);
+                    if (prev_pointer->compare_exchange_weak(not_marked_cur, node_marked_ptr(next.get(), 0))) {
                         delete_node(cur);
                     } else {
                         back_off();
@@ -243,7 +253,7 @@ namespace lu {
             return find(&head_, value, pos, compare);
         }
 
-        bool insert_node(node_pointer new_node) {
+        bool insert_node(node_ptr new_node) {
             BackOff back_off;
             position pos;
             while (true) {
@@ -265,7 +275,7 @@ namespace lu {
 
         template<class _ValueType, class = std::enable_if_t<is_key_value>>
         bool insert(const key_type &key, _ValueType &&value) {
-            node_pointer new_node = new Node(key, std::forward<_ValueType>(value));
+            node_ptr new_node = new node_type(key, std::forward<_ValueType>(value));
             if (!insert_node(new_node)) {
                 delete new_node;
                 return false;
@@ -275,7 +285,7 @@ namespace lu {
 
         template<class... Args>
         bool emplace(Args &&...args) {
-            node_pointer new_node = new Node(std::forward<Args>(args)...);
+            node_ptr new_node = new node_type(std::forward<Args>(args)...);
             if (!insert_node(new_node)) {
                 delete new_node;
                 return false;
@@ -311,7 +321,7 @@ namespace lu {
             lu::hazard_pointer head_guard = lu::make_hazard_pointer();
             position pos;
             while (true) {
-                auto head = head_guard.protect(head_, [](node_marked_pointer ptr) { return ptr.get(); });
+                auto head = head_guard.protect(head_, [](node_marked_ptr ptr) { return ptr.get(); });
                 if (!head) {
                     break;
                 }
@@ -340,7 +350,7 @@ namespace lu {
         }
 
     private:
-        std::atomic<node_marked_pointer> head_{};
+        std::atomic<node_marked_ptr> head_{};
     };
 
     template<class KeyType, class ValueType>
